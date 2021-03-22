5bf3c16a80dfff361fd3b746fe5ea0d9c08afc18
cbsanjaya <9c96b6@gmail.com>
2021-03-22 14:34:08 +0700

    init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+    <title>Rumah</title>
+</head>
+<body>
+    <h3>NIM: 200769040027</h3>
+    <h1>NAMA: Cahya Bagus Sanjaya</h1>
+</body>
+</html>
