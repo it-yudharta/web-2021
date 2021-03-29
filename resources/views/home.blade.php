f88a885149cb4289e04633ad4ccac0c7aeab8030
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2021-03-29 14:25:49 +0700

    basic css dan post method

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,16 @@
     <title>Rumah</title>
 </head>
 <body>
-    <h3>NIM: 200769040027</h3>
-    <h1>NAMA: Cahya Bagus Sanjaya</h1>
+    <h3 style="color: blue;text-align: center;">NIM: 200769040027</h3>
+    <h1 style="text-align: center">NAMA: Cahya Bagus Sanjaya</h1>
+
+    <form method="POST" action="/home" style="margin-left:50px">
+        @csrf
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
 </body>
 </html>
