01101d4ba8cb54287e233ddc60b75bf2b4a0dfb6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2021-05-03 16:04:56 +0700

    pakai bootstrap

--- resources/views/penduduk.blade.php
+++ resources/views/penduduk.blade.php
@@ -1,29 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Penduduk</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Penduduk</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+  rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
-    <table>
+  <div class="container">
+    <table class="table">
+      <thead>
         <tr>
-            <th>NIP</th>
-            <th>Nama</th>
-            <th>TTL</th>
-            <th>Alamat</th>
+          <th>NIP</th>
+          <th>Nama</th>
+          <th>TTL</th>
+          <th>Alamat</th>
         </tr>
+      </thead>
+      <tbody>
         @foreach ($penduduk as $p)
         <tr>
-            <td>{{ $p->nip }}</td>
-            <td>{{ $p->nama }}</td>
-            <td>{{ $p->ttl }}</td>
-            <td>{{ $p->alamat }}</td>
+          <td>{{ $p->nip }}</td>
+          <td>{{ $p->nama }}</td>
+          <td>{{ $p->ttl }}</td>
+          <td>{{ $p->alamat }}</td>
         </tr>
         @endforeach
-    </table>
+      </tbody>
+  </table>
 
     {{ $penduduk->links() }}
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>
